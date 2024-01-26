ec9452fceba91596c4644c3d5d2beddd1441088a
Fh Ndiritu <fhndiritu@gmail.com>
2024-01-26 11:07:38 +0300

    truncating seed tables

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,12 @@
-
+tables =%w[datasets splits prompts]
+tables.each do |table|
+    ActiveRecord::Base.connection.execute("TRUNCATE TABLE #{table} RESTART IDENTITY CASCADE;")
+end
 
 3.times do 
     dataset = FactoryBot.create(:dataset)
-    FactoryBot.create(:split, dataset: dataset)
+    split = FactoryBot.create(:split, dataset: dataset)
+    6.times do 
+        FactoryBot.create(:prompt, split: split)
+    end
 end
